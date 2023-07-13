a302c45a970b8e64fa44dad44c0c359dda701436
Filipe Valente Gomes <filipego@gmail.com>
2023-07-13 16:41:03 -0400

    added logo

--- components/common/Header.js
+++ components/common/Header.js
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from 'next/image'
 
 import Container from "./Container"
 import Nav from "./Nav"
@@ -8,7 +9,14 @@ function Header() {
         <header className="header">
             <Container classes="flex-space-between pt-2">
                 <Link href="/">
-                    <a className="logo">Dique Media</a>
+                    <a className="logo">
+                        <Image
+                            src="/Dique_Media_Website_Images/DIQUE-MEDIA-logo.png?updatedAt=1689120199856"
+                            alt="Dique Media Logo"
+                            width={100}
+                            height={100}
+                        />
+                    </a>
                 </Link>
                 <Nav />
             </Container>
