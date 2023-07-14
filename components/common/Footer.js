295305c913be1ed669305f1aaec3d4f8c7a2b7e4
Filipe Valente Gomes <filipego@gmail.com>
2023-07-14 17:13:41 -0400

    made changes to social icons

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -26,9 +26,46 @@ function Footer() {
 
                     <nav className="social">
                         <ul className="flex-space-between">
-                            <li><a href="#" target="_blank" rel="noreferrer">Social 1</a></li>
-                            <li><a href="#" target="_blank" rel="noreferrer">Social 2</a></li>
-                            <li><a href="#" target="_blank" rel="noreferrer">Social 3</a></li>
+                            <li>
+                                <a href="https://www.tiktok.com/@diquemedia" target="_blank" rel="noreferrer">
+                                    <Image
+                                        src="/Dique_Media_Website_Images/tiktok.png?updatedAt=1689368578826"
+                                        alt="TikTok"
+                                        width={24}
+                                        height={24}
+                                    />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.youtube.com/@sarahvegamelian" target="_blank" rel="noreferrer">
+                                    <Image
+                                        src="/Dique_Media_Website_Images/youtube-logo.png?updatedAt=1689368578839"
+                                        alt="YouTube"
+                                        width={24}
+                                        height={24}
+                                    />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.instagram.com/diquemedia/" target="_blank" rel="noreferrer">
+                                    <Image
+                                        src="/Dique_Media_Website_Images/instagram.png?updatedAt=1689368578965"
+                                        alt="Instagram"
+                                        width={24}
+                                        height={24}
+                                    />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/_sarahvega_" target="_blank" rel="noreferrer">
+                                    <Image
+                                        src="/Dique_Media_Website_Images/twitter.png?updatedAt=1689368578879"
+                                        alt="Twitter"
+                                        width={24}
+                                        height={24}
+                                    />
+                                </a>
+                            </li>
                         </ul>
                     </nav>
                 </div>
