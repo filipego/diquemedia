103d63d80eb4e40b9e15795b693a2dbc1aa6b679
Filipe Valente Gomes <filipego@gmail.com>
2023-07-14 18:29:13 -0400

    worked on cta

--- components/common/ContactCTA.js
+++ components/common/ContactCTA.js
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 import Container from "./Container"
 
@@ -13,7 +14,17 @@ function ContactCTA() {
             <div className="contactCTA">
                 <h2>let us take you further than you've ever been.</h2>
                 <Link href="/contact">
-                    <a className="contact-cta-btn">Contact Us</a>
+                    <a className="contactCTA__btn">
+                        <div className="icon">
+                            <Image
+                                src="/Dique_Media_Website_Images/up-right.png?updatedAt=1689373412999"
+                                alt="Arrow Up Right"
+                                width={30}
+                                height={30}
+                            />
+                        </div>
+                        <p>Contact Us</p>
+                    </a>
                 </Link>
             </div>
 
