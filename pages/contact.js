93db92cc4af0ec9cfb7ccbe5c962c0d71a060f54
Filipe Valente Gomes <filipego@gmail.com>
2023-08-01 13:59:52 -0400

    added notif

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import React, { useRef, useState, useEffect } from 'react';
-import emailjs from '@emailjs/browser';
 import Image from 'next/image'
 import Container from "../components/common/Container"
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function Contact({ }) {
@@ -11,6 +13,16 @@ function Contact({ }) {
 
     useEffect(() => {
         document.body.className = "contact"
+    });
+
+    const notify = () => toast.success("Thank you, your message has been sent", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
     });
 
     const sendEmail = (e) => {
@@ -25,7 +37,7 @@ function Contact({ }) {
                 // console.log(error.text);
             });
 
-
+        notify()
         e.target.reset();
     };
 
@@ -121,6 +133,19 @@ function Contact({ }) {
                     </form>
                 </div>
             </Container>
+            <div>
+                <ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                />
+            </div>
 
         </>
     )
